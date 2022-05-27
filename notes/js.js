77262443603623aee207fe55e9b61aaa04591a8f
Hermann AGOSSOU <hermannagossou6@gmail.com>
2022-05-27 23:24:20 +0100

    Create js.js

---
+++ notes/js.js
@@ -0,0 +1,121 @@
+var DICT = [
+{"id":0, "title":"Le B du problem solving","author":"H.A","url":"#","date":"27/05/2022",
+"text":`Tout le monde peut analyser un problème et trouver des solutions. 
+        Comprendre le problème est un excellent levier vers sa résolution.`,
+"field":"theorèmes du moi"},
+{"id":1, "title":"Le génie entrepreneur","author":"H.A","url":"#","date":"27/05/2022",
+"text":`Anyone could come up with any idea. But not everyone have the gut to fit them into the reality framework, that complex world to unravel.`,
+"field":"theorèmes du moi"},
+{"id":0, "title":"Le génie entrepreneur","author":"H.A","url":"#","date":"27/05/2022",
+"text":`Un entrepreneur est un chercheur de problème, de solution et de marché ( quand est-ce que le problème pourrait avoir lieu). C'est un problem-solveur, de même qu'un matheux, un philosophe ou un programmeur.
+
+`,
+"field":"theorèmes du moi"},
+{"id":0, "title":"L'esprit critique'","author":"H.A","url":"#","date":"27/05/2022",
+"text":`Notre perception est biaisée par nos prejugés et notre manque d'ouverture au monde. Le premier arme de lentrepreneur, c'est la recherche de solution à des problèmes. Ce mindset permet même de trouver des défauts à des solutions que d'autres ont désigné pour un problème précis: c'est de la critique constructive. Une manière de développer son esprit critique est d'écrire ( sur un support qu'on sera obligé de lire regulièrement) que "les questions sont plus importantes que les réponses
+       `,
+"field":"theorèmes du moi"},
+{"id":0, "title":"Consolider les acquis","author":"H.A","url":"#","date":"27/05/2022",
+"text":`Souvent, on cherche solution à des problèmes. Mais, quand ces problèmes se répètent, on compte sur sa mémoire ( ou expérience) pour retrouver la solution: c'est l'expérience. On peut aussi compter sur des notes, des documents ( physiques ou non ) ou des gens. Mais, on ne se demande pas pourquoi est-ce-que la solution a été oublié.
+Parfois, on oublie la solution ; ou soit on oublie que le problème a été traité; ou soit des problèmes sont similaires et on ne s'en rend pas compte.
+Il y a un moyen de répondre à ces trois défauts 
+Après chaque solution trouvée, on peut se demander avec quelle manière de penser on pouvais trouver la même solution sans trop dépenser de ressources ( temps, ...).  Souvent, répondre à cette question est simple et est facile à stocker dans une mémoire long-terme.
+Ainsi, si on change chaque fois, sa manière de trouver des réponses en fonction de ce self-feedback, on devient, plus outillé avec le temps. 
+`,
+"field":"-----------"},
+{"id":0, "title":"Je cherche la connaissance ","author":"H.A","url":"#","date":"27/05/2022",
+"text":`Voir (system unravelement ou system breakown ou reverse engineering ou proposition d'explication, ) , c'est d'abord 
+- avoir confiance en soi,
+- faire des hypothèses, les utiliser
+- accepter les correlations en tant que telles ( et faire la différence entre correlation et causalité )
+- utiliser à ce qui devrait être. Comprendre cette phrase de son point de vue est un bon départ !
+- C'est aussi savoir écouter pour apprendre de tout. Apprendre de tout car la remarque, la curiosité et la remise en question permettent de découvrir des causes ou conséquences de certains phénomènes que le chercheur de connaissance a besoin de comprendre.
+`,
+"field":"Depasser la peur de se tromper"},
+{"id":0, "title":"Depasser la peur de se tromper","author":"H.A","url":"#","date":"27/05/2022",
+"text":`Le "je ne sais pas" n'est pas tellement une réponse dans un contexte d'innovation car ça signifie qu'on a pas la réponse. Mais, ce n'est pas parce-qu'on n'a pas la réponse qu'on n'a pas des idées sur ce que la réponse pourrait être. 
+De même, à défaut d'avoir une information, on peut faire un guess et ajouter "peut-être". Ça aide beaucoup en innovation et en travaux d'équipes quand l'athmosphère encourage cette forme de prise d'initiatives. Ex: le brainstorming. Autrement, celà peut être perçu comme du bavardage.
+`,
+"field":"theorèmes du moi"},
+{"id":0, "title":"Le génie entrepreneur","author":"H.A","url":"#","date":"27/05/2022",
+"text":`Un entrepreneur est un chercheur de problème, de solution et de marché ( quand est-ce que le problème pourrait avoir lieu). C'est un problem-solveur, de même qu'un matheux, un philosophe ou un programmeur.
+
+`,
+"field":"theorèmes du moi"},
+{"id":0, "title":"La consistance: partie 1","author":"H.A","url":"#","date":"27/05/2022",
+"text":`A home is based on some core piliars. Any field of physics is based on some core principles. Having some piliars ( core principles) to this home 🏡 (unified framework) optimize learning, comprehension and assessment of new informations. 
+For a human, these core principles exist as references to later statements that will be truth relative to these principles. Then, there are two types of informations in this context:  core principles and relative thruth.
+In general, people keep adding informations into their knowledge set. For example, many students do not think about link linking subjects learnt from various classes.
+Instead of adding informations into our knowledge set, reassesing our core principles make us consistant. Changing these core principles overtime make a difference between an young age growing and an old one stagnant.  
+You can also be a writer. It helps in consistency because you may need a clear story to make a point. You can talk and forget, think and forget. But, writing put your old worldwiews in front of you. Then, you need to explain the difference. That's how you grow in consistency.
+In general rule, as long as you remain curious about something, you will tend to understand it better
+`,
+"field":"theorèmes du moi"},
+{"id":0, "title":"La consistance: partie 2","author":"H.A","url":"#","date":"27/05/2022",
+"text":`On reçoit chaque jour des règles, le comment de certaines choses. Mais, peu de personnes prennent le temps de savoir le "pourquoi" telle ou telle règle. Moi, avant de mettre une leçon dans ma base de règles, je m'assure que la nouvelle règle est en consistance avec les autres. Ainsi, j'ai un set d'hypothèses qui règlent ma conduite. A cause de celà, je boycotte des règles morales, des traditions tant que je ne suis pas sûr qu'ils soient importants.
+`,
+"field":"theorèmes du moi"},
+{"id":0, "title":"Retenir les concepts plutôt que les raisonnements","author":"H.A","url":"#","date":"27/05/2022",
+"text":`Dans une histoire, retenir les leçons, sinon les chercher. Tu verras que plusieurs histoires véhiculent les mêmes leçons
+Dans la solution à un exercise, retenir un moyen de penser à cette solution; la créer sinon. Tu verras que plusieurs exercises peuvent être résolues avec un nombre plus limité de méthodes.
+Dans un système de théories, retenir les hypothèses de bases, sinon, les former.
+Dans une base de connaissance, classer les problèmes par leurs causes racines. Tu verras que plusieurs problèmes ont les mêmes causes. 
+`,
+"field":"theorèmes du moi"},
+{"id":0, "title":"Quand on s'embrouille","author":"H.A","url":"#","date":"27/05/2022",
+"text":`Voilà une occasion de questionner ses hypothèses. 
+`,
+"field":"theorèmes du moi"},
+
+
+];
+
+if(typeof(String.prototype.trim) === "undefined")
+{
+String.prototype.trim = function() 
+{
+    return String(this).replace(/^\s+|\s+$/g, '');
+};
+}
+var mygroup = document.getElementById("my-group");
+DICT.forEach((elt, id)=>{
+var W30 = document.createElement('div');
+W30.classList.add("w-30");
+var CTN = document.createElement('div');
+CTN.classList.add("container");
+h1 = document.createElement('h3');
+h1.style["text-align"] = "center"
+h1.textContent = elt["title"]
+h1.style["color"] = "blue"
+h1.classList.add("btn");
+//h1.classList.add("btn-info");
+h1.classList.add("collapsible");
+
+p1 = document.createElement('p');
+p1.textContent = "----------"
+p1.style["text-align"] = "center"
+p1.style["color"] = "rgba(255,0,0,0.5)"
+p1.style["font-size"] = "15px"
+p1.style["margin-bottom"] = "0px"
+
+blockquote = document.createElement('blockquote');
+//blockquote.classList.add("content");
+//blockquote.scrollHeight = "10px"
+p2 = document.createElement('p');
+p2.innerHTML = "\""+elt["text"].trim().replaceAll('\n',"<div style='heigth:2px'></div>")+"\""
+//p2.style["font-size"] = "15px"
+//p2.style["fcolor"] = "#333"
+
+
+footer = document.createElement('footer');
+footer.innerHTML = elt["author"] + ", " + "<a href='" + elt["url"] + "'>" + elt["field"] + "</a>" + ", " + elt["date"]
+
+
+blockquote.appendChild(p2);
+blockquote.appendChild(footer);
+
+CTN.appendChild(h1);
+CTN.appendChild(p1);
+CTN.appendChild(blockquote);
+mygroup.appendChild(CTN)
+})
